f5d065e7cc4130117d37050e90056dad1c039365
AbhinavB <bhatnagarnick@gmail.com>
2025-05-18 13:32:44 +0530

    updated start_logger function, now the logging path is not mandatory. if logging path will not be given, only console logs will be shown.

--- logs/logger_setup.py
+++ logs/logger_setup.py
@@ -38,42 +38,48 @@ class CustomFormatter(logging.Formatter):
         return formatter.format(record)
 
 
-def start_logger(logger_path: str, file_log_level=logging.INFO, console_log_level=logging.INFO):
-    if logger_path[logger_path.find('.', 1):].lower() in ['.txt', '.log']:
-        filename = logger_path
-    else:
-        default_file_name = f'{datetime.now().strftime("%Y-%m-%d")}.log'
-        filename = os.path.join(logger_path, default_file_name)
-
+def start_logger(logger_path: str = None, file_log_level=logging.INFO, console_log_level=logging.INFO):
+    # initialising logger
     lgr = logging.getLogger('my_logger')
+
+    # changing the default logging level to debug from warning
     lgr.setLevel(logging.DEBUG)
 
-    # create file handler that logs info and higher level messages
-    file_handler = logging.FileHandler(filename, encoding='utf-8')
-    file_handler.setLevel(file_log_level)
+    if logger_path is not None:
+        if logger_path[logger_path.find('.', 1):].lower() in ['.txt', '.log']:
+            filename = logger_path
+        else:
+            default_file_name = f'{datetime.now().strftime("%Y-%m-%d")}.log'
+            filename = os.path.join(logger_path, default_file_name)
+
+        # create file handler that logs info and higher level messages
+        file_handler = logging.FileHandler(filename, encoding='utf-8')
+        file_handler.setLevel(file_log_level)
+
+        # create formatter and add it to the handlers
+        formatter = logging.Formatter(
+            fmt='{asctime} | {filename:23} | Line {lineno:4} | {levelname:8} | {message}',
+            style='{')
+
+        file_handler.setFormatter(formatter)
+
+        # add the handlers to logger
+        lgr.addHandler(file_handler)
 
     # create console handler that logs info and higher level messages
     console_handler = logging.StreamHandler()
     console_handler.setLevel(console_log_level)
 
-    # create formatter and add it to the handlers
-    formatter = logging.Formatter(
-        fmt='{asctime} | {filename:23} | Line {lineno:4} | {levelname:8} | {message}',
-        style='{')
-
     console_handler.setFormatter(CustomFormatter())
-    file_handler.setFormatter(formatter)
 
     # add the handlers to logger
     lgr.addHandler(console_handler)
-    lgr.addHandler(file_handler)
-
-    # time.sleep(2)
-    return None
 
 
 if __name__ == '__main__':
-    start_logger('my_test_logger.log', console_log_level=logging.DEBUG)
+    # start_logger('my_test_logger.log', console_log_level=logging.DEBUG)
+    start_logger(console_log_level=logging.DEBUG)
+
     logger = logging.getLogger('my_logger')
 
     logger.debug('this is debug data')
